0c9c1c34ebf9d3065f2f6e50b546c1d575d0e7f0
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-11-27 00:26:18 +0530

    Added code for retrofit http call

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,32 +3,43 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 public class Main {
 
     // Proper main method
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
+    public static void main (String[] args) throws IOException {
+//        OkHttpClient client = new OkHttpClient();
+//        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//
+//        try {
+//            String result = run(client, url);
+//            System.out.println(result);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+         Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        try {
-            String result = run(client, url);
-            System.out.println(result);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        TodoService todoService = retrofit.create(TodoService.class);
+        Todo t = todoService.getTodoById("5").execute().body();
+
+        System.out.println("Todo object downloaded is" + t.toString());
     }
 
     // run() method MUST be outside main()
-    public static String run(OkHttpClient client, String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
+//    public static String run(OkHttpClient client, String url) throws IOException {
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        try (Response response = client.newCall(request).execute()) {
+//            return response.body().string();
+//        }
+//    }
 }
